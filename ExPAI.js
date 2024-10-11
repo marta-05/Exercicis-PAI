26204330955d7129caaafa3e9defd4dc50d8cdf1
marta-05 <marta.franch.buisan@gmail.com>
2024-10-11 18:39:47 +0200

    primer

---
+++ ExPAI.js
@@ -0,0 +1,27 @@
+//4.2.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+let a = 10;
+let b = 20;
+let c = 5;
+a = a + 3;
+b = b + 4 - a;
+c = a + b + c;
+a = a + c;
+b = 4;
+c = c + 3 - b + 2;
+console.log(a,b,c);
+
+// 4.3.- Quins valors s'escriuran a la consola després d'executar aquest codi?
+// Escriu-los en un paper i desprès executa el codi. Et donen el mateix?
+a = 5;
+b = 18;
+c = 15;
+d = 22;
+a = a + 10;
+b = b + 5 - c;
+c = c + 4 + b;
+d = d + b + a;
+a = a + 1;
+b = b + c;
+c = b + c;
+d = b + b;
+console.log(a,b,c,d);
